7a463ddfdb6764676dee5edeeed9aa36e851f4d2
Derek Cannon <derektcannon@gmail.com>
2025-12-05 05:03:59 -0800

    Add test layout
    Import layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
+<script lang="ts">
+	import layout from '$lib/layouts/colemak-dh.json';
+
+	function printPretty(data, splitCol = 5) {
+		const rows = {};
+
+		Object.entries(data.keys).forEach(([key, info]) => {
+			if (!rows[info.row]) rows[info.row] = [];
+			rows[info.row][info.col] = key;
+		});
+
+		const out = Object.keys(rows)
+			.sort((a, b) => a - b)
+			.map((row) => {
+				const r = rows[row];
+				return (
+					r.slice(0, splitCol).join(' ') +
+					'  ' + // extra gap between hands
+					r.slice(splitCol).join(' ')
+				);
+			})
+			.join('\n');
+
+		return out;
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<pre>{printPretty(layout)}</pre>
